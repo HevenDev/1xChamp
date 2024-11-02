2ab4b2f0c014e5f3e984eb8e9e26b3c536305336
HevenDev <hevendevofficial@gmail.com>
2024-11-02 14:19:29 +0530

    second Deployment

--- controllers/auth.Controllers.js
+++ controllers/auth.Controllers.js
@@ -40,7 +40,7 @@ export const register = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000,  // 1 day
   });
 
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
@@ -96,7 +96,7 @@ export const logout = (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',  // Enable in production
-    sameSite: 'strict',
+    sameSite: 'None',
   });
   
   return res.status(200).json({ message: 'Logged out successfully' });
